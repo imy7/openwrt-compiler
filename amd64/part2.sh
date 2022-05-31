751e63c8cad064205a8605cb8b4572c455aefcbe
imy7 <34760601+imy7@users.noreply.github.com>
2022-05-31 10:24:28 +0800

    Update part2.sh

--- amd64/part2.sh
+++ amd64/part2.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 # replace default theme to argon
-rm -rf feeds/luci/themes/luci-theme-bootstrap/
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci*/Makefile
+#rm -rf feeds/luci/themes/luci-theme-bootstrap/
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci*/Makefile
 
 # Modify DHCP client for iptv
-sed -i 's/\${vendorid:+-V "$vendorid"}/-V \"\"/g' package/network/config/netifd/files/lib/netifd/proto/dhcp.sh
+#sed -i 's/\${vendorid:+-V "$vendorid"}/-V \"\"/g' package/network/config/netifd/files/lib/netifd/proto/dhcp.sh
+sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/imy7/g' package/base-files/files/bin/config_generate
+sed -i 's/ssid=OpenWrt/ssid=imy7/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/option enabled '0'/option enabled '1'/g' feeds/packages/multimedia/minidlna/files/minidlna.config
+sed -i 's/option enabled		0/option enabled		1/g' feeds/packages/net/miniupnpd/files/upnpd.config
+sed -i 's/option igdv1		1/option igdv1		0/g' feeds/packages/net/miniupnpd/files/upnpd.config
