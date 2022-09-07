42c73a12cac87d59c41611e7fe161a053354dfd9
imy7 <34760601+imy7@users.noreply.github.com>
2022-09-07 11:43:00 +0800

    Update part1.sh

--- amd64/part1.sh
+++ amd64/part1.sh
@@ -4,3 +4,4 @@ svn co https://github.com/coolsnowwolf/lede/trunk/tools/ucl tools/ucl
 svn co https://github.com/coolsnowwolf/lede/trunk/tools/upx tools/upx
 sed -i 'N;24a\tools-y += ucl upx' tools/Makefile
 sed -i 'N;40a\$(curdir)/upx/compile := $(curdir)/ucl/compile' tools/Makefile
+echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
