a84d767c697e47103d0dc1a356862fe3a26b01f5
imy7 <34760601+imy7@users.noreply.github.com>
2022-09-09 09:54:02 +0800

    Update part1.sh

--- R4S/part1.sh
+++ R4S/part1.sh
@@ -4,4 +4,6 @@ svn co https://github.com/coolsnowwolf/lede/trunk/tools/ucl tools/ucl
 svn co https://github.com/coolsnowwolf/lede/trunk/tools/upx tools/upx
 sed -i 'N;24a\tools-y += ucl upx' tools/Makefile
 sed -i 'N;40a\$(curdir)/upx/compile := $(curdir)/ucl/compile' tools/Makefile
-echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
+echo 'src-git ikoolproxy https://github.com/yaof2/luci-app-ikoolproxy' >>feeds.conf.default
+echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
+echo 'src-git bypass https://github.com/kiddin9/openwrt-bypass' >>feeds.conf.default
