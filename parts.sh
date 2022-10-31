b8630b63060e180dc41ac40e12adc4e0be7e18a0
imy7 <34760601+imy7@users.noreply.github.com>
2022-10-31 15:01:23 +0800

    Update parts.sh

--- parts.sh
+++ parts.sh
@@ -6,5 +6,3 @@ sed -i 's/ImmortalWrt/imy7/g' package/base-files/files/bin/config_generate
 sed -i 's/ssid=OpenWrt/ssid=imy7/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i 's/option enabled '0'/option enabled '1'/g' feeds/packages/multimedia/minidlna/files/minidlna.config
-sed -i 's/option enabled		0/option enabled		1/g' feeds/packages/net/miniupnpd/files/upnpd.config
-sed -i 's/option igdv1		1/option igdv1		0/g' feeds/packages/net/miniupnpd/files/upnpd.config
