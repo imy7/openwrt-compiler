d1c2a09fc9ee0b42150d6c93834807c78022785e
imy7 <34760601+imy7@users.noreply.github.com>
2022-11-01 13:08:36 +0800

    Update part1.sh

--- R2S/part1.sh
+++ R2S/part1.sh
@@ -4,9 +4,9 @@ svn co https://github.com/coolsnowwolf/lede/trunk/tools/ucl tools/ucl
 svn co https://github.com/coolsnowwolf/lede/trunk/tools/upx tools/upx
 sed -i 'N;24a\tools-y += ucl upx' tools/Makefile
 sed -i 'N;40a\$(curdir)/upx/compile := $(curdir)/ucl/compile' tools/Makefile
-git clone https://github.com/yaof2/luci-app-ikoolproxy package/new/luci-app-ikoolproxy
-git clone https://github.com/kiddin9/luci-app-dnsfilter package/new/luci-app-dnsfilter
-git clone https://github.com/sbwml/luci-app-mosdns package/new/luci-app-mosdns
-git clone https://github.com/kiddin9/openwrt-bypass package/new/luci-app-bypass
-git clone https://github.com/siwind/luci-app-wolplus package/new/luci-app-wolplus
-git clone https://github.com/imy7/luci-theme package/new/theme
+git clone https://github.com/yaof2/luci-app-ikoolproxy package/new
+git clone https://github.com/kiddin9/luci-app-dnsfilter package/new
+git clone https://github.com/sbwml/luci-app-mosdns package/new
+git clone https://github.com/kiddin9/openwrt-bypass package/new
+git clone https://github.com/siwind/luci-app-wolplus package/new
+git clone https://github.com/imy7/luci-theme package/new
