c4adcc6f6c0a35a35a00095264bf5d5e58196aad
imy7 <34760601+imy7@users.noreply.github.com>
2022-11-01 13:36:44 +0800

    Update part3.sh

--- R2S/part3.sh
+++ R2S/part3.sh
@@ -1,1 +1,8 @@
-
+#!/bin/bash
+cd openwrt/package
+git clone https://github.com/yaof2/luci-app-ikoolproxy 
+git clone https://github.com/kiddin9/luci-app-dnsfilter
+git clone https://github.com/sbwml/luci-app-mosdns
+git clone https://github.com/kiddin9/openwrt-bypass
+git clone https://github.com/siwind/luci-app-wolplus
+git clone https://github.com/imy7/luci-theme
