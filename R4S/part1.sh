dcb2bdfa2f8a7b2d2fe509201c14514b770253f1
imy7 <34760601+imy7@users.noreply.github.com>
2022-11-01 13:23:39 +0800

    Update part1.sh

--- R4S/part1.sh
+++ R4S/part1.sh
@@ -5,9 +5,9 @@ svn co https://github.com/coolsnowwolf/lede/trunk/tools/upx tools/upx
 sed -i 'N;24a\tools-y += ucl upx' tools/Makefile
 sed -i 'N;40a\$(curdir)/upx/compile := $(curdir)/ucl/compile' tools/Makefile
 #echo 'src-git ikoolproxy https://github.com/yaof2/luci-app-ikoolproxy' >>feeds.conf.default
-git clone https://github.com/yaof2/luci-app-ikoolproxy package/new
-git clone https://github.com/kiddin9/luci-app-dnsfilter package/new
-git clone https://github.com/sbwml/luci-app-mosdns package/new
-git clone https://github.com/kiddin9/openwrt-bypass package/new
-git clone https://github.com/siwind/luci-app-wolplus package/new
-git clone https://github.com/imy7/luci-theme package/new
+git clone https://github.com/yaof2/luci-app-ikoolproxy package/new/ikoolproxy
+git clone https://github.com/kiddin9/luci-app-dnsfilter package/new/dnsfilter
+git clone https://github.com/sbwml/luci-app-mosdns package/new/mosdns
+git clone https://github.com/kiddin9/openwrt-bypass package/new/bypass
+git clone https://github.com/siwind/luci-app-wolplus package/new/wolplus
+git clone https://github.com/imy7/luci-theme package/new/theme
